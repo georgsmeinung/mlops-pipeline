c2e370a2cc6ee57dc02b2dea932cfc4eb64394cb
Jorge Nicolau <22195389+georgsmeinung@users.noreply.github.com>
2025-09-23 12:02:44 -0300

    Add script to automate Jupyter kernel setup

---
+++ scripts/setup_jupyter_kernel.bat
@@ -0,0 +1,12 @@
+REM Automatización del la generación del kernel de jupyter por etapa
+REM Parámetros:
+REM      %1: Directorio del venv en el que se basará el kernel
+REM      %2: Nombre con el que kernel se visualizará en Jupyter
+REM Uso: scripts\setup_jupyter_kernel.bat venv_ingesta Ingesta
+@echo off
+set VENV_DIR=%1
+set KERNEL_NAME=%2
+call %VENV_DIR%\Scripts\activate
+pip install ipykernel
+python -m ipykernel install --user --name=%KERNEL_NAME% --display-name="Python (%KERNEL_NAME%)"
+echo Kernel %KERNEL_NAME% configurado para %VENV_DIR%
