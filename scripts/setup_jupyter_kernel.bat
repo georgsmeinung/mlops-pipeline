cc29962c0fffd9316e8a7e3778797e0234034492
Jorge Nicolau <22195389+georgsmeinung@users.noreply.github.com>
2025-09-23 12:03:01 -0300

    Fix formatting and add newline at end of script

--- scripts/setup_jupyter_kernel.bat
+++ scripts/setup_jupyter_kernel.bat
@@ -3,6 +3,7 @@ REM Parámetros:
 REM      %1: Directorio del venv en el que se basará el kernel
 REM      %2: Nombre con el que kernel se visualizará en Jupyter
 REM Uso: scripts\setup_jupyter_kernel.bat venv_ingesta Ingesta
+
 @echo off
 set VENV_DIR=%1
 set KERNEL_NAME=%2
